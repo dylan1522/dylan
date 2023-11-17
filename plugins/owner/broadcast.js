ee54b0ea25083f0ff34d11e10388e2b529a0591b
DrkBotBase <marziosincro@gmail.com>
2023-11-17 08:35:28 -0500

    update

--- plugins/owner/broadcast.js
+++ plugins/owner/broadcast.js
@@ -9,16 +9,12 @@ module.exports = {
   check: { pts: 1 },
   async handler(m, {myBot, text}) {
     if (!text) return m.reply('Que quieres enviar?');
-    let res = await fetchJson(process.env.DB_OLD);
-    let db = await Object.keys(res);
-    m.reply(`Enviar difusión a ${db.length} chat.\n*Tiempo de envio:* Aproximadamente ${db.length * 3.5} segundos.`);
+    let dataBase = await fetchJson(process.env.DB_OLD);
+    let numbers = await Object.keys(dataBase);
+    m.reply(`Enviar difusión a ${numbers.length} chat.\n*Tiempo de envio:* Aproximadamente ${numbers.length * 3.5} segundos.`);
 
-    for (let i of db) {
-      await myBot.sendImage(
-        i,
-        await m.quoted.download() || global.thumb,
-        text
-      );
+    for (let number of numbers) {
+      await myBot.sendText(number, text);
       await sleep(1500);
     }
     m.reply("Difusion Enviada")
