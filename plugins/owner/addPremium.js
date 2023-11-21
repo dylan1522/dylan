67002a287f722c5ebbe1a94d7fc41366ad3735c0
DrkBotBase <marziosincro@gmail.com>
2023-11-21 08:38:22 -0500

    add command fixed

--- plugins/owner/addPremium.js
+++ plugins/owner/addPremium.js
@@ -6,13 +6,13 @@ module.exports = {
   async handler(m, {myBot, args, User}) {
     let newPrem = args[0]+'@s.whatsapp.net';
     let plan = args[1] ? args[1] : 'bronce';
-    try {
+    if (await User.check(newPrem)) {
       await User.activatePremiumPlan(newPrem, plan);
       let checkUser = await User.show(newPrem);
       /*let emojis = {"bronce": "🥉", "plata": "🥈", "oro": "🥇", "semana": "7️⃣"};
       let premiumEmoji = emojis[checkUser.plan];*/
       myBot.sendText(newPrem, `Ya eres Premium\nPlan: ${args[1]}`);
-    } catch {
+    } else {
       m.reply('Usuario no encontrado!');
     }
   }
